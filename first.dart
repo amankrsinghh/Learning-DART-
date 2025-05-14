2474854c77aaf95804b6397cafa9236bfef93cc2
xaman <xaman282@gmail.com>
2025-05-14 13:18:54 +0530

    introduction dart

---
+++ first.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void fun () {
+  print("Captain Bauhat chai peeti hai ");
+}
+void main(){
+//   print("Enter Your Name : ");
+//   String? name = stdin.readLineSync();
+//   print(name);
+// /////////////=================================/////////////////////
+//   stdout.write(" please enter your name: ");
+//   String name2 = stdin.readLineSync()!;
+//   print(name2);
+
+  for(int i = 1;i<=10;i++){
+    print(i);
+  }
+  var name = "aman";
+  print(name);
+
+}

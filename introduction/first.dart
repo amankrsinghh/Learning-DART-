644874c6c1261c83806eb9ac9424e0107bdc4740
xaman <xaman282@gmail.com>
2025-05-16 22:47:35 +0530

    introduction dart

--- introduction/first.dart
+++ introduction/first.dart
@@ -1,21 +1,33 @@
+
+import 'dart:ffi';
 import 'dart:io';
 
-void fun () {
-  print("Captain Bauhat chai peeti hai ");
-}
-void main(){
-//   print("Enter Your Name : ");
-//   String? name = stdin.readLineSync();
-//   print(name);
-// /////////////=================================//////////////////////
-//   stdout.write(" please enter your name: ");
-//   String name2 = stdin.readLineSync()!;
-//   print(name2);
+void main() {
+    // print("Enter Your Name : ");
+    // String? name = stdin.readLineSync();
+    // print(name);
+  // /////////////=================================//////////////////////
+  //   stdout.write(" please enter your name: ");
+  //   String name2 = stdin.readLineSync()!;
+  //   print(name2);
 
-  for(int i = 1;i<=10;i++){
-    print(i);
+  // for(int i = 1;i<=10;i++){
+  //   print(i);
+  // }
+  // var name = "aman";
+  // print(name);
+  // int a = 20;
+  // double b = 30.345;
+  // print(b);
+
+
+
+class human{
+  void fun(){
+    var name = stdin.readLineSync();
+    var roll = stdin.readByteSync();
+    print(name);
+    print(roll);
   }
-  var name = "aman";
-  print(name);
 
 }
